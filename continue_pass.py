b11167a5bdd0c30797b57819adc65fffd9cbbadd
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-11-02 00:53:15 +0530

    Add files via upload

---
+++ continue_pass.py
@@ -0,0 +1,42 @@
+# n=0
+# while n<10:
+#     n+=1
+#     if n==5:
+#         continue
+#     print(n)              # except 5 all numbers from 1 to 10 will be printed line by line
+
+
+# n=0
+# while n<10:
+#     n+=1
+#     if n>5:
+#         continue
+#     print(n)            # will print 1 to 5 line by line
+
+
+# n=0
+# while n<10:
+#     n+=1
+#     if n<5:
+#         continue
+#     print(n)              # will print 5 to 10 line by line
+
+
+# n=0
+# while n<10:
+#     n+=1
+#     if n<=5:
+#         continue
+#     print(n)                # will print 6 to 10 line by line
+
+
+# if (11>2):
+#     pass                    # pass will not print anything eventhough the condition is True. It just pass the statement/condition
+# else:
+#     print('False')
+
+
+# if (11<2):
+#     pass                    
+# else:
+#     print('False')          # False
